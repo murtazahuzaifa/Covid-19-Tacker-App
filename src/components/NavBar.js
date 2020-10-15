000a7bfb8ff7afd34ad89356db9912ddb48e3379
murtazahuzaifa <murtaza.huzaifa2@gmail.com>
2020-10-15 21:38:51 +0500

    navbar updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import GitHub from '@material-ui/icons/GitHub';
+// import IconButton from '@material-ui/core/IconButton';
+// import GitHub from '@material-ui/icons/GitHub';
 // import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
@@ -27,12 +27,8 @@ export default function ButtonAppBar() {
         <AppBar position="static">
             <Toolbar className={classes.toolBar}>
                 <Typography variant="h6" className={classes.title}>
-                    COVID-19 Tracker App
+                    COVID-19 Tracker Graph
                     </Typography>
-                <IconButton color="inherit" aria-label="github-link" className={classes.gitHub}
-                    href="https://github.com/murtazahuzaifa/Covid-19-Tacker-App" target='_blank'>
-                    <GitHub />
-                </IconButton>
             </Toolbar>
         </AppBar>
     );
